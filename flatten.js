610b4cb8962f53058aa57698bf3ed61798cd987e
Lauren <laurenkaitlyn@gmail.com>
2023-02-26 14:42:03 -0500

    eslint

--- flatten.js
+++ flatten.js
@@ -4,7 +4,7 @@ const assertArrayEqual = function(array1, array2) {
   } else {
     console.log("These 2 strings do not match.");
   }
-}
+};
 
 const eqArrays = function(arr1, arr2) {
   return arr1.toString() === arr2.toString();
@@ -16,6 +16,6 @@ const flatten = function(arr) {
 
   newArr = arr.flat(1);
   return newArr;
-}
+};
 
 console.log(flatten([1, 2, [3, 4], 5, [6]]));// => [1, 2, 3, 4, 5, 6]
