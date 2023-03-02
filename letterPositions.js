1133c8a32ba3cb4d8efdb741f34598cc82a9cffc
Lauren <laurenkaitlyn@gmail.com>
2023-03-02 14:23:40 -0500

    get index of letters in a string

---
+++ letterPositions.js
@@ -0,0 +1,26 @@
+const letterPositions = function(sentence) {
+  let results = {};
+  //taking away spaces
+  sentence = sentence.replace(/\s+/g,'');
+
+  for (let i = 0; i < sentence.length; i++) {
+    results[sentence[i]] = results[sentence[i]] || [];
+    results[sentence[i]].push(i);
+  }
+
+  return results;
+}
+
+const assertArrayEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("These 2 arrays are the same.");
+  } else {
+    console.log("These 2 strings do not match.");
+  }
+}
+
+const eqArrays = function(arr1, arr2) {
+  return arr1.toString() === arr2.toString();
+};
+
+assertArrayEqual(letterPositions("hello").e, [1]);
