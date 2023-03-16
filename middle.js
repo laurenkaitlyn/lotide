4a0f36e43a62575b9c8f9c28f59b655ba77704c4
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 14:14:26 -0400

    cleaned up code and added more comments

--- middle.js
+++ middle.js
@@ -1,20 +1,19 @@
-const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArrays')
-
 
 const middle = function(array) {
   let midArr = [];
   let mid = array[Math.floor((array.length / 2))];
   
+  //for arrays with 1 or 2 elements return empty array
   if (array.length <= 2) {
     return midArr
-  } else if (array.length % 2 !== 0) {
+  }
+  //for arrays with odd numbers of elements an array with 1 middle element is returned
+  if (array.length % 2 !== 0) {
     midArr.push(array[mid - 1]);
-  } else if (array.length % 2 === 0) {
-    midArr.push(array[mid - 2]);
-    console.log(midArr)
-    midArr.push(array[mid - 1]);
-    console.log(midArr)
+  }
+  //for arrays with even numbers return an array with the 2 middle elements
+  if (array.length % 2 === 0) {
+    midArr.push(array[mid - 2], array[mid - 1]);
   }
   return midArr;
 }
