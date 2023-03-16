bc6b5309d8b1c0d46b9b29b26e8415ac31233741
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 13:39:27 -0400

    modularizing assertEqual

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,6 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
-};
+const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
@@ -9,4 +7,7 @@ const assertArrayEqual = function(array1, array2) {
     console.log("These 2 strings do not match.");
   }
 }
+
+module.exports = assertArrayEqual;
+
 
