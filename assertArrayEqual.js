617051430b7730ded8ffe94cbce7384e7b652e79
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 14:15:41 -0400

    show what the test arrays were

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -2,9 +2,10 @@ const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
-    console.log("These 2 arrays are the same.");
-  } else {
-    console.log("These 2 strings do not match.");
+    console.log(`[${array1}] and [${array2}] are the same.`);
+  }
+  if (eqArrays(array1, array2) === false) {
+    console.log(`[${array1}] and [${array2}] are NOT the same.`);
   }
 }
 
