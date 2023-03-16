d599f8c0cc651b1258b3bf2cd32290f6a028df67
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 12:33:49 -0400

    modularizing eqArrays & taking out tests

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,6 @@ const eqArrays = function(arr1, arr2) {
   return arr1.toString() === arr2.toString();
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+
 
 module.exports = eqArrays;
