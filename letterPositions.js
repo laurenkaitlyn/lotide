68188a5b9a90502724afcd5c9208afaa1a0720e0
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 21:55:41 -0400

    took out testing and assert functions

--- letterPositions.js
+++ letterPositions.js
@@ -11,18 +11,4 @@ const letterPositions = function(sentence) {
   return results;
 }
 
-const assertArrayEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log("These 2 arrays are the same.");
-  } else {
-    console.log("These 2 strings do not match.");
-  }
-}
-
-const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
-};
-
-assertArrayEqual(letterPositions("hello").e, [1]);
-
 module.exports = letterPositions;
