fca417aeb6c01cb6e93885a4827e67f224e086cf
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 21:35:59 -0400

    took out tests and moved them to test folder

--- flatten.js
+++ flatten.js
@@ -1,14 +1,3 @@
-const assertArrayEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log("These 2 arrays are the same.");
-  } else {
-    console.log("These 2 strings do not match.");
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
-};
 
 //used .flat with degree 1 to flatten array
 const flatten = function(arr) {
@@ -17,7 +6,5 @@ const flatten = function(arr) {
   newArr = arr.flat(1);
   return newArr;
 };
-
-console.log(flatten([1, 2, [3, 4], 5, [6]]));// => [1, 2, 3, 4, 5, 6]
 
 module.exports = flatten;
