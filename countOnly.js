f09427c572fbd5cb64685705e23b8f41981cba23
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 21:39:25 -0400

    took out testing and assertEqual function

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`Assertion Passed ✅✅✅: ${actual} === ${expected}\n`);
-  } else {
-    return (`Assertion Failed 🛑🛑🛑: ${actual} !== ${expected}\n`);
-  }
-};
 
 /*countOnly takes an array and an object 
 and returns an object that counts everything 
 that was input */
 
 // allItems: array of strings
-//itemsToCount: onject specifying what to count
+//itemsToCount: object specifying what to count
 
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
@@ -28,26 +21,5 @@ const countOnly = function(allItems, itemsToCount) {
 
   return results;
 }
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-console.log(
-  assertEqual(result1["Jason"], 1),
-  assertEqual(result1["Karima"], undefined),
-  assertEqual(result1["Fang"], 2),
-  assertEqual(result1["Agouhanna"], undefined)
-);
 
 module.exports = countOnly;
