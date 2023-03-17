1e228e68db6143adb4cfe21b37e801107f4851df
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 21:37:04 -0400

    updating index with more helper functions

---
+++ index.js
@@ -0,0 +1,25 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const flatten = require('./flatten');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  flatten,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  without
+};
