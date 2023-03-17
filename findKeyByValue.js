c1358f67f030d7a1c58c8fb7a2683a053ae01929
Lauren <laurenkaitlyn@gmail.com>
2023-03-16 21:55:19 -0400

    took out testing and assert functions

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`Assertion Passed ✅✅✅: ${actual} === ${expected}`);
-  } else {
-    return (`Assertion Failed 🛑🛑🛑: ${actual} !== ${expected}`);
-  }
-};
 
 const findKeyByValue = function (object, value) {
   /* returns the first element in the provided by Object.keys 
@@ -12,17 +5,5 @@ const findKeyByValue = function (object, value) {
   If no values satisfy the testing function, undefined is returned. */
   return Object.keys(object).find(key => object[key] === value);
 }
-
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-console.log(
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"), '\n',
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined)
-);
 
 module.exports = findKeyByValue;
