f6b6a8e8e20d81656bfa7738c8b35dbcee7468be
Lauren <laurenkaitlyn@gmail.com>
2023-03-18 22:40:15 -0400

    fixed some bugs with the function

--- eqObjects.js
+++ eqObjects.js
@@ -1,14 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
-};
 
-const assertEquals = function(actual, expected) {
-  if (actual === expected) {
-    return (`Assertion Passed ✅✅✅: ${actual} === ${expected}`);
-  } else {
-    return (`Assertion Failed 🛑🛑🛑: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
 // first check if one object has more keys than the other
@@ -17,66 +8,28 @@ const eqObjects = function(object1, object2) {
 
   //comparing lengths
   if (obj1Array.length !== obj2Array.length) {
-    return false
-  }
-  //loop through the keys comparing the values
-  for (let value of obj1Array) {
-    if (Array.isArray(object2)) {
-      if(!eqArrays(object1[value], object2[value])) {
-        return false
+    return false;
+  } else {
+   
+    for (let key of obj1Array) {
+      if (Array.isArray(object1.key && Array.isArray(object2.key))) {
+        if (!eqArrays(object1.key, object2.key)) {
+          return false;
+        }
+      
+      } else if (typeof object1.key === 'object' && typeof object2.key === 'object') {
+        if(!eqObjects(object1.key, object2.key)) {
+          return false
+        }
+      } else {
+        if (object1.key !== object2.key) {
+          return false
+        }
       }
-  } else if (object1[value] !== object2[value]) {
-    return false
-  }
-
-  return true
+    }
+    return true;
   }
 };
-//first round of testing with primitives
-const shirtObject = {
-  colour: "red",
-  size: "medium"
-};
 
-const anotherShirtObject = {
-  size: "medium",
-  colour: "red"
-};
 
-const longSleeveShirtObject = {
-  size: "medium",
-  colour: "red",
-  sleeveLength: "long"
-};
-
-console.log(
-  assertEquals(eqObjects(shirtObject, anotherShirtObject), true),
-  '\n',
-  assertEquals(eqObjects(shirtObject, longSleeveShirtObject), false),
-  '\n'
-);
-
-//second round of testing with arrays
-const multiColourShirtObject = {
-  colours: ["red", "blue"],
-  size: "medium"
-};
-
-const anotherMultiColourShirtObject = {
-  size: "medium",
-  colours: ["red", "blue"]
-};
-
-const longSleeveMultiColourObject = {
-  size: "medium",
-  colours: ["red", "blue"],
-  sleeveLength: "long"
-};
-
-//assert equal
-console.log(
-  assertEquals(eqObjects(multiColourShirtObject, anotherMultiColourShirtObject), true),
-  '\n',
-  assertEquals(eqObjects(multiColourShirtObject, longSleeveMultiColourObject), false),
-  '\n'
-);
+module.exports = eqObjects;
