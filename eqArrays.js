16ea15e4b05aa39e62d2a8435b86561c45566ae8
Lauren <laurenkaitlyn@gmail.com>
2023-04-03 23:08:24 -0400

    fixing for loop

--- eqArrays.js
+++ eqArrays.js
@@ -3,12 +3,12 @@ const eqArrays = function(arr1, arr2) {
 
   if (arr1.length !== arr2.length) false;
 
-  for (let index = 0; index < arr1.length; index ++) {
-    if (Array.isArray(arr1[index])) && (Array.isArray(arr2[index])) {
+  for (let i = 0; i < arr1.length; i ++) {
+    if ((Array.isArray(arr1[i])) && (Array.isArray(arr2[i]))) {
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
-    } else if (arr[i] !== arr2[i]) {
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
