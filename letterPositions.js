2728328a3e5d92fe2a5d827033c6eb7770952862
Lauren <laurenkaitlyn@gmail.com>
2023-04-03 22:25:46 -0400

    change so original array is not modified

--- letterPositions.js
+++ letterPositions.js
@@ -1,14 +1,13 @@
 const letterPositions = function(sentence) {
   let results = {};
-  //taking away spaces
-  sentence = sentence.replace(/\s+/g,'');
 
   for (let i = 0; i < sentence.length; i++) {
-    results[sentence[i]] = results[sentence[i]] || [];
-    results[sentence[i]].push(i);
+    if (sentence[i] !== " ") {
+      results[sentence[i]] ? results[sentence[i]].push(i): results[sentence[i]] = [i];
+    }
   }
 
   return results;
-}
+};
 
 module.exports = letterPositions;
